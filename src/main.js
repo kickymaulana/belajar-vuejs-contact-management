d008932d66213bd6f89770d10b9f599c92eedaf1
kickymaulana <kickymaulana@gmail.com>
2025-08-10 08:15:51 +0700

    menambahkan UserRegister dan router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Layout from "./components/Layout.vue";
+import UserRegister from "./components/User/UserRegister.vue"
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+
+  routes: [
+    {
+      component: Layout,
+      children: [
+        {
+          path: "/register",
+          component: UserRegister
+        }
+      ]
+    },
+    {
+      path: "/dashboard"
+    }
+  ]
+
+})
+
+
+createApp(App).use(router).mount('#app')
